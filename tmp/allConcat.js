978fa642a063c757775f134466d479aaa4451582
Alex Lee and Chris Carr <alex+chris@>
2017-04-17 16:46:15 -0700

    add

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -20,3 +20,20 @@ $(document).ready(function(){
     $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
   });
 });
+
+$(document).ready(function(){
+  $('#time').text(moment());
+});
+
+var apiKey = "d6719c52bc4e98b28dd9060daf9f68f4";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      console.log(JSON.stringify(response));
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
